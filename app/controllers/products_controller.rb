bc8da2bc0d43f182e194fd2103dd1231fab9b485
Sunanda <sunanda@Sunandas-MacBook-Air.local>
2015-08-31 10:18:25 -0400

    init

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
-    respond_to do |format|
+                  respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
